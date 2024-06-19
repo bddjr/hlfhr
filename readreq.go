1db47a9d57d4debc8218f14d8cab046a0f9a57b0
bddjr <bddjrcn@outlook.com>
2024-06-19 22:28:05 +0800

    Fix regexp

--- readreq.go
+++ readreq.go
@@ -9,7 +9,7 @@ import (
 
 // "GET /index.html HTTP/1.1\r\nHost: localhost:5678\r\nUser-Agent: curl/8.7.1\r\nAccept: */*\r\n\r\n"
 // ["GET /index.html HTTP/1.1\r\nHost: localhost:5678\r" "/index.html" "localhost:5678"]
-var compiledRegexp_ReadReq = regexp.MustCompile(`^[A-Z]{3,7} (/\S*) HTTP/1.[01]\r\nHost: (\S+)\r`)
+var compiledRegexp_ReadReq = regexp.MustCompile(`^[A-Z]{3,7} (/\S*) HTTP/1\.[01]\r\nHost: (\S+)\r`)
 
 // Parse the request Host header and path from Hlfhr_HttpOnHttpsPortErrorHandler.
 // Suppose this request using HTTP/1.1
