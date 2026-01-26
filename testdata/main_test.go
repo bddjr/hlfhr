30313c395164657c9cf8ea520c7d1898225f543b
bddjr <bddjr08@outlook.com>
2026-01-26 17:21:51 +0800

    test HlfhrHandler

--- testdata/main_test.go
+++ testdata/main_test.go
@@ -18,6 +18,7 @@ import (
 )
 
 func request(serverAddr string) {
+	println("request")
 	// requestBody := make([]byte, 8192)
 	// mathrand.Read(requestBody)
 
@@ -37,6 +38,7 @@ func request(serverAddr string) {
 			return nil
 		},
 	}
+	defer client.CloseIdleConnections()
 
 	// Intentionally occupy a connection to test whether
 	// the server can handle requests in parallel.
@@ -103,8 +105,47 @@ func request(serverAddr string) {
 		}
 		println()
 	}
+}
 
-	client.CloseIdleConnections()
+func requestTestHlfhrHandler(serverAddr string) {
+	println("requestTestHlfhrHandler")
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	err := http2.ConfigureTransport(transport)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{
+		Transport: transport,
+		Timeout:   time.Second,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			println("Redirect")
+			return nil
+		},
+	}
+	defer client.CloseIdleConnections()
+
+	httpURL := "http://" + serverAddr + "/test?a=b&c=d"
+	println(httpURL)
+	println()
+	// requestBodyReader := bytes.NewReader(requestBody)
+	req, err := http.NewRequest("GET", httpURL, nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	print(string(respBody))
+
+	println()
 }
 
 func test1(serverAddr string) {
@@ -113,11 +154,15 @@ func test1(serverAddr string) {
 	srv := hlfhr.New(&http.Server{
 		Addr: serverAddr,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
+			if r.Method == "HEAD" {
+				return
+			}
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 			enc := json.NewEncoder(w)
 			enc.SetEscapeHTML(false)
 			tlsVersion, _ := strings.CutPrefix(tls.VersionName(r.TLS.Version), "TLS ")
 			err := enc.Encode(map[string]any{
+				"Method":         r.Method,
 				"Proto":          r.Proto,
 				"TLS_Version":    tlsVersion,
 				"TLS_ServerName": r.TLS.ServerName,
@@ -152,6 +197,27 @@ func test1(serverAddr string) {
 		addr += ":80"
 		request(addr)
 	}
+
+	srv.HlfhrHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "HEAD" {
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		enc := json.NewEncoder(w)
+		enc.SetEscapeHTML(false)
+		err := enc.Encode(map[string]any{
+			"Method": r.Method,
+			"Proto":  r.Proto,
+			"IsTLS":  r.TLS != nil,
+			"Host":   r.Host,
+			"URI":    r.RequestURI,
+			// "RequestHeader":  r.Header,
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+	requestTestHlfhrHandler(serverAddr)
 
 	println("Shutdown")
 	err = srv.Shutdown(context.Background())
